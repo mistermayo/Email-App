f2bf8d1f0a5d34ecfdcf4b3dcc3ee1f5b794e728
Tucker <tuckerjamesmayo@gmail.com>
2015-03-06 15:46:23 -0800

    perform cleanup and remove folders controller

--- controllers/folders.js
+++
@@ -1,9 +0,0 @@
-// Email.FoldersController = Ember.ObjectController.extend({
-//   actions: {
-//     delete: function() {
-//       if (confirm('Click OK to delete this folder')) {
-//         this.get('model').destroyRecord();
-//       }
-//     }
-//   }
-// });
